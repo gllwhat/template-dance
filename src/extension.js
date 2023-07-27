edc048eed57476e491b3573cf6a545bb9198bfb7
shankui <shankui@dianplus.cn>
2023-07-27 20:07:20 +0800

    提交代码

---
+++ src/extension.js
@@ -0,0 +1,289 @@
+const vscode = require("vscode");
+const { Template, TemplateDanceTreeDataProvider } = require("./templateClass");
+
+function activate(context) {
+  const templateDanceTreeDataProvider = new TemplateDanceTreeDataProvider(
+    context
+  );
+  const templateDanceTreeView = vscode.window.createTreeView(
+    "templateDanceTreeView",
+    {
+      treeDataProvider: templateDanceTreeDataProvider,
+      showCollapseAll: true,
+      canSelectMany: true,
+    }
+  );
+
+  // 添加
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "template-dance.addTemplate",
+      async (item) => {
+        let parent;
+        if (templateDanceTreeDataProvider.getSelectId() || item) {
+          let template = item
+            ? item
+            : templateDanceTreeDataProvider.getItem(
+              templateDanceTreeDataProvider.getSelectId()
+            );
+          parent = await vscode.window.showInputBox({ value: template.name, prompt: "请输入父节点名称", });
+        } else {
+          parent = await vscode.window.showInputBox({
+            prompt: "请输入父节点名称",
+          });
+        }
+        let id;
+        console.log("add", parent);
+        if (parent) {
+          let res = templateDanceTreeDataProvider.initChildId(parent);
+          console.log("add ====> ", res, typeof res === "object");
+          if (typeof res === "object") {
+            id = res.childId;
+            parent = res.parentId;
+          } else {
+            vscode.window.showErrorMessage(`添加失败，不存在'${parent}'节点`);
+            return;
+          }
+        } else {
+          id = templateDanceTreeDataProvider.initId();
+        }
+        const name = await vscode.window.showInputBox({
+          prompt: "请输入模板名称",
+        });
+        if (!name) {
+          return;
+        }
+        if (!templateDanceTreeDataProvider.validateName(name)) {
+          vscode.window.showErrorMessage("添加失败，模板重名");
+          return;
+        }
+        let content = await vscode.window.showInputBox({
+          prompt: "请输入模板内容",
+          ignoreFocusOut: true,
+        });
+        if (!content) {
+          return;
+        }
+        content = content === undefined ? "" : content.replace(/ /g, '&nbsp;').replace(/\n/g, '<br>');
+        const template = new Template(id, name, content, parent);
+        console.log("template =====>", template);
+        templateDanceTreeDataProvider.add(template);
+      }
+    )
+  );
+
+  // 编辑
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "template-dance.editTemplate",
+      async (item) => {
+        // let template = templateDanceTreeDataProvider.getItem(templateDanceTreeDataProvider.selectId)
+        let name = await vscode.window.showInputBox({ value: item.name, prompt: "请输入模板名称", });
+        name = name === undefined ? "" : name
+        if (!item.name) {
+          return;
+        }
+        let content = await vscode.window.showInputBox({
+          value: item.content, prompt: "请输入模板内容", ignoreFocusOut: true,
+        });
+        content = content === undefined ? "" : content.replace(/ /g, '&nbsp;');
+        console.log('edit content', content)
+        templateDanceTreeDataProvider.edit(item.id, name, content);
+      }
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("template-dance.hello", () => {
+      vscode.window.showInformationMessage("Hello");
+    })
+  );
+
+  // 选中
+  const changeSelectionFun = (
+    e
+  ) => {
+    console.log("11 onDidChangeSelection e ", e);
+    if (e.selection.length > 0) {
+      const template = e.selection[0];
+      templateDanceTreeDataProvider.setSelectId(template.id);
+      vscode.commands.executeCommand("setContext", "templateItemContext", true);
+    } else {
+      vscode.commands.executeCommand(
+        "setContext",
+        "templateItemContext",
+        false
+      );
+    }
+  };
+  templateDanceTreeView.onDidChangeSelection(changeSelectionFun);
+
+  // 查看
+  context.subscriptions.push(
+    vscode.commands.registerCommand("template-dance.viewTemplate", (item) => {
+      // let template = templateDanceTreeDataProvider.getItem(templateDanceTreeDataProvider.selectId)
+      const panel = vscode.window.createWebviewPanel(
+        "view",
+        `view: ${item.name}`,
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true, // 允许运行脚本
+          retainContextWhenHidden: false, // 隐藏时保留内容状态
+        }
+      );
+      let content = item.content === undefined ? "" : item.content
+      panel.webview.html = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+          body {
+            background-color: #fff;
+            color: #333;
+          }
+        </style>
+      </head>
+      <body>
+        <div id="content">
+        <div>
+          <p><span style="font-weight: bold;font-size: 18px">name: </span> <span>${item.name}</span></p>
+        </div> 
+        <div>
+          <p style="font-weight: bold;font-size: 18px">content:</p>
+          <pre id="view">${content.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/(&nbsp;){2,}/g, "</br>")}</pre>
+        </div>
+      </div>
+      <script>
+        let content = document.getElementById("content"),view = document.getElementById("view"),textarea = document.getElementById("textarea")
+      </script>
+      </body>
+    </html>`;
+    })
+  );
+
+  // 删除
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "template-dance.deleteTemplate",
+      async (item) => {
+        // let template = templateDanceTreeDataProvider.getItem(templateDanceTreeDataProvider.selectId)
+        console.log("template =====>", item);
+        templateDanceTreeDataProvider.delete(item);
+      }
+    )
+  );
+
+  // 使用
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "template-dance.useTemplate",
+      async (item) => {
+        // let template = templateDanceTreeDataProvider.getItem(templateDanceTreeDataProvider.selectId)
+        console.log("template =====>", item);
+        // 获取当前激活的编辑器
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+          // 获取光标所在位置
+          const position = editor.selection.active;
+          // 创建编辑器修改对象
+          const edit = new vscode.WorkspaceEdit();
+          // 在光标所在位置插入文本
+          let content = item.content.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&nbsp;/g, ' ').replace(/<br>/g, '\n')
+          edit.insert(editor.document.uri, position, content);
+          // 应用修改
+          await vscode.workspace.applyEdit(edit);
+          const selection = new vscode.Selection(
+            position,
+            editor.selection.active
+          );
+          await vscode.commands.executeCommand(
+            "editor.action.formatSelection",
+            selection
+          );
+        }
+      }
+    )
+  );
+
+  // search
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "template-dance.searchTemplate",
+      async () => {
+        let name = await vscode.window.showInputBox({
+          prompt: "请输入模板名称关键字",
+        });
+        name = name === undefined ? "" : name
+        templateDanceTreeDataProvider.search(name);
+      }
+    )
+  );
+
+  // refresh
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "template-dance.refreshTemplate",
+      async () => {
+        templateDanceTreeDataProvider.refresh();
+      }
+    )
+  );
+
+  // import
+  context.subscriptions.push(
+    vscode.commands.registerCommand("template-dance.import", async () => {
+      const fileUris = await vscode.window.showOpenDialog({
+        canSelectFiles: true,
+        canSelectFolders: false,
+        canSelectMany: false,
+        filters: {
+          "Tree Files": ["json"],
+        },
+      });
+      console.log("import fileUris", fileUris);
+      const fileUri = fileUris ? fileUris[0] : undefined;
+      if(!fileUri) {
+        return
+      }
+      const fileContent = await vscode.workspace.fs.readFile(fileUri);
+      const treeData = JSON.parse(fileContent.toString());
+      console.log("import treeData", treeData);
+      console.log(
+        "import validateTreeData",
+        templateDanceTreeDataProvider.validateTreeData(treeData)
+      );
+    })
+  );
+
+  // revoke
+  context.subscriptions.push(
+    vscode.commands.registerCommand("template-dance.revoke", async () => {
+      let templatesOld = templateDanceTreeDataProvider.getTemplatesOld();
+      console.log("revoke templatesOld", templatesOld);
+      if (templatesOld.length) {
+        templateDanceTreeDataProvider.revoke();
+      }
+    })
+  );
+
+  // export
+  context.subscriptions.push(
+    vscode.commands.registerCommand("template-dance.export", async () => {
+      const fileUri = await vscode.window.showSaveDialog({
+        defaultUri: vscode.Uri.file("templates.json"),
+        filters: {
+          "JSON Files": ["json"],
+        },
+      });
+      if (!fileUri) {
+        return;
+      }
+      const templates = templateDanceTreeDataProvider.getTemplates();
+      const json = JSON.stringify(templates, null, 2);
+      await vscode.workspace.fs.writeFile(fileUri, Buffer.from(json));
+      vscode.window.showInformationMessage("Templates exported successfully!");
+    })
+  );
+}
+module.exports = { activate };
---
+++ src/templateClass.js
@@ -0,0 +1,498 @@
+const vscode = require('vscode');
+
+
+class Template {
+  constructor(id, name, content, parent, children, hasChild) {
+    this.id = id;
+    this.name = name;
+    this.content = content;
+    this.parent = parent;
+    this.children = children;
+    this.hasChild = hasChild;
+  }
+
+  toData() {
+    return {
+      id: this.id,
+      name: this.name,
+      content: this.content,
+      parent: this.parent,
+      children: this.children,
+      hasChild: this.children && this.children.length > 0
+    };
+  }
+
+  static fromData(data) {
+    if (data.hasChild) {
+      return new Template(data.id, data.name, data.content, data.parent, data.children, data.hasChild);
+    } else {
+      return new Template(data.id, data.name, data.content, data.parent);
+    }
+  }
+}
+
+class TemplateDanceTreeDataProvider {
+  constructor(context) {
+    this.context = context;
+    this.templates = [];
+    this.templatesOld = [];
+    this.selectId = "";
+    this.importTemMap = new Map();
+    this._onDidChangeTreeData = new vscode.EventEmitter();
+    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+    this.loadTemplates();
+  }
+
+  loadTemplates() {
+    const savedTemplates = this.context.globalState.get('templates', []);
+    console.log("loadTemplates savedTemplates", savedTemplates);
+    this.templates = savedTemplates.map(Template.fromData);
+    console.log("loadTemplates this.templates", this.templates);
+  }
+
+  saveTemplates() {
+    const data = this.templates.map(t => t.toData());
+    this.context.globalState.update('templates', data);
+    console.log("saveTemplates this.templates", this.templates);
+  }
+
+  async addTemplate(template) {
+    this.templates.push(template);
+    this.saveTemplates();
+    this.refresh();
+  }
+
+  async updateTemplate(template) {
+    const index = this.templates.findIndex(t => t.id === template.id);
+    if (index !== -1) {
+      this.templates[index] = template;
+      this.saveTemplates();
+      this.refresh();
+    }
+  }
+
+  async deleteTemplate(template) {
+    const index = this.templates.findIndex(t => t.id === template.id);
+    if (index !== -1) {
+      this.templates.splice(index, 1);
+      this.saveTemplates();
+      this.refresh();
+    }
+  }
+
+  getItem(id) {
+    let stack = [...this.templates];
+    while (stack.length) {
+      let node = stack.shift();
+      if (!node) {
+        continue;
+      }
+      if (node.id === id) {
+        return node;
+      }
+      if (node.hasChild && node.children instanceof Array) {
+        const stackTemplates = stack.filter(item => item instanceof Template).map(item => item);
+        const childTemplates = (node.children || []).filter(item => item instanceof Template).map(item => item);
+        stack = [...stackTemplates, ...childTemplates];
+      }
+    }
+  }
+
+  getParent(template) {
+    if (!template.parent) {
+      return null;
+    }
+    return this.getItem(template.parent);
+  }
+
+  async getChildren(template) {
+    console.log("getChildren template", template);
+    if (template) {
+      return Promise.resolve((template.children || []).map(child => {
+        return child instanceof Template ? child : Template.fromData(child);
+      }));
+    } else {
+      return Promise.resolve(this.templates);
+    }
+  }
+
+  getTreeItem(element) {
+    console.log('getTreeItem element', element);
+    let tooltip = element.content && element.content.length > 20 ? `${element.content.slice(0, 20)}...` : element.content;
+    return {
+      id: element.id,
+      label: element.name,
+      tooltip: tooltip,
+      children: element.children || null,
+      collapsibleState: !element.hasChild ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Collapsed,
+      iconPath: vscode.Uri.file(this.context.asAbsolutePath('./src/icon/icon-light-code2.svg')),
+    };
+  }
+
+  // 校验数据的字段、类型
+  checkType(obj, typeData) {
+    let res = "";
+    let keyArr = Object.keys(obj);
+    for (let i = 0; i < keyArr.length; i++) {
+      let idx = typeData.findIndex(item => {
+        let result = false;
+        if (item.key === keyArr[i]) {
+          if (item.type === "array") {
+            result = obj[keyArr[i]] instanceof Array;
+          } else {
+            result = typeof obj[keyArr[i]] === item.type;
+          }
+        } else {
+          result = false;
+        }
+        return result;
+      });
+      if (idx === -1) {
+        res = `${res}1`;
+      } else {
+        res = `${res}0`;
+      }
+    }
+    console.log('checkType res', res);
+
+    if (res.length !== typeData.length || res.includes("1")) {
+      return false;
+    } else {
+      return true;
+    } 
+  }
+
+  // 校验数据是否合法
+  // 包括parent、hasChild、children
+  checkData(data, msg = "") {
+    console.log("checkData data", data);
+    let res = msg;
+    if (this.importTemMap.has(data.id) || this.importTemMap.has(data.name)) {
+      res = `${res}${res ? "。" : ""} ${data.id}或${data.name}重复。`;
+    } else {
+      this.importTemMap.set(data.id, 1);
+      this.importTemMap.set(data.name, 1);
+    }
+    if (data.hasChild && data.children.length === 0) {
+      res = `${res} ${res ? "。" : ""} ${JSON.stringify({ id: data.id, name: data.name })}`;
+    }
+    if (data.hasChild) {
+      for (let i = 0; i < data.children.length; i++) {
+        if (data.children[i].parent && data.children[i].parent !== data.id) {
+          res = `${res} ${res ? "。" : ""} ${JSON.stringify({ id: data.children[i].id, name: data.children[i].name })}`;
+        }
+        this.checkData(data.children[i], res);
+      }
+    }
+    console.log("checkData res", res);
+    return res;
+  }
+
+  formatTreeData(treeData) {
+    if (!treeData) {
+      return;
+    }
+    for (let i = 0; i < treeData.length; i++) {
+      treeData[i] = new Template(treeData[i].id, treeData[i].name, treeData[i].content, treeData[i].parent, treeData[i].children, treeData[i].hasChild);
+      if (treeData[i].hasChild) {
+        this.formatTreeData(treeData[i].children);
+      }
+    }
+    return treeData;
+  }
+
+  validateTreeData(treeData) {
+    let typeData = [
+      { key: 'id', type: 'string' },
+      { key: 'name', type: 'string' },
+      { key: 'content', type: 'string' },
+      { key: 'parent', type: 'string' },
+      { key: 'children', type: 'array' },
+      { key: 'hasChild', type: 'boolean' },
+    ];
+    let errorMsg = "", errorMsg2 = "";
+    if (!(treeData instanceof Array)) {
+      vscode.window.showErrorMessage(`添加失败，传入数据类型有误`);
+      return;
+    }
+    // 校验数据
+    for (let i = 0; i < treeData.length; i++) {
+      let res = this.checkType(treeData[i], typeData);
+      console.log('check result ', res);
+      if (!res) {
+        errorMsg = `${errorMsg}${errorMsg ? "。" : ""} ${JSON.stringify(treeData[i])}`;
+
+      }
+      let res2 = this.checkData(treeData[i]);
+      if (res2) {
+        errorMsg2 = `${errorMsg2}${errorMsg2 ? "。" : ""} ${res2}`;
+      }
+    }
+    if (errorMsg) {
+      vscode.window.showErrorMessage(`添加失败，以下数据字段或数据类型有误：${errorMsg}`);
+      return;
+    }
+    if (errorMsg2) {
+      vscode.window.showErrorMessage(`添加失败，以下数据hasChild/children/parent字段有误：${errorMsg2}`);
+      return;
+    }
+  }
+
+  importTree(treeData) {
+    this.templatesOld = this.templates;
+    this.templates = [...this.templates, ...treeData];
+    console.log('import this.templates', this.templates);
+    this.saveTemplates();
+    this.refresh();
+  }
+
+  revoke() {
+    this.templates = [...this.templatesOld];
+    this.templatesOld = [];
+    this.saveTemplates();
+    this.refresh();
+  }
+
+  validateName(name) {
+    console.log("validateName name", name);
+    const stack = [...this.templates];
+    console.log("validateName stack", this.templates, stack);
+    let res = true;
+    while (stack.length) {
+      const node = stack.pop();
+      console.log("validateName node", node);
+      if (node.name === name) {
+        return false;
+      }
+      if (node.children && node.children.length > 0) {
+        for (let i = node.children.length - 1; i >= 0; i--) {
+          stack.push(node.children[i]);
+        }
+      }
+    }
+    console.log("validateName res", res);
+    return res;
+  }
+
+  initId() {
+    return (this.templates.length + 1) + "";
+  }
+
+  initChildId(parentName) {
+    let childId = null;
+    const stack = [...this.templates];
+    const parentNode = [];
+    console.log("initChildId stack", stack);
+    while (stack.length) {
+      const node = stack.pop();
+      if (node.name === parentName) {
+        parentNode.push(node);
+        break;
+      }
+      console.log("initChildId node", node);
+      console.log("initChildId node.hasChild ", node.hasChild);
+      console.log("initChildId node.children.length ", node.children?.length);
+
+      if (node.hasChild && node.children.length > 0) {
+        for (let i = node.children.length - 1; i >= 0; i--) {
+          stack.push(node.children[i]);
+        }
+      }
+      console.log("initChildId stack", stack);
+    }
+    console.log("initChildId parentNode", parentNode);
+
+    if (parentNode.length === 1) {
+      childId = parentNode[0].children ? parentNode[0].children.length : 0;
+      return {
+        childId: `${parentNode[0].id}-${childId}`,
+        parentId: parentNode[0].id
+      };
+    } else {
+      return false;
+    }
+  }
+
+  refresh() {
+    console.log('refresh templates', this.templates);
+    this.loadTemplates();
+    this._onDidChangeTreeData.fire();
+  }
+
+  add(template) {
+    console.log('add template', template);
+    if (template.parent) {
+      const stack = [...this.templates];
+      while (stack.length) {
+        const node = stack.pop();
+        console.log('add node', node);
+        if (node.id === template.parent) {
+          node.hasChild = true;
+          if (node.children) {
+            node.children.push(template);
+          } else {
+            node.children = [template];
+          }
+          break;
+        }
+        if (node.children && node.children.length > 0) {
+          for (let i = node.children.length - 1; i >= 0; i--) {
+            stack.push(node.children[i]);
+          }
+        }
+      }
+    } else {
+      this.templates.push(template);
+    }
+    console.log('add this.templates', this.templates);
+    this.saveTemplates();
+    this.refresh();
+  }
+
+  editFun(templates, id, content) {
+    for (let i = 0; i < templates.length; i++) {
+      if (templates[i].id === id) {
+        templates[i].content = content;
+        this.saveTemplates();
+        this.refresh();
+        return;
+      }
+      if (templates[i].hasChild) {
+        this.editFun(templates[i].children, id, content);
+      }
+    }
+  }
+
+  edit(id, name, content) {
+    this.editFun(this.templates, id, content);
+  }
+
+  deleteFun(element, _templates, parentNode) {
+    if (!element) {
+      return;
+    }
+    let templates = _templates;
+    let index = templates.findIndex(template => template.id === element.id);
+    console.log('delete index', index);
+    console.log('delete _templates', _templates);
+    if (index === -1) {
+      for (let i = 0; i < templates.length; i++) {
+        if (templates[i].children) {
+          this.deleteFun(element, templates[i].children, templates[i]);
+        }
+      }
+    } else {
+      _templates.splice(index, 1);
+      if (_templates.length === 0) {
+        parentNode.hasChild = false;
+      }
+    }
+  }
+
+  delete(template) {
+    this.deleteFun(template, this.templates);
+    this.saveTemplates();
+    this.refresh();
+  }
+
+  search(keyword) {
+    if (!keyword) {
+      // 如果没有关键词，则显示所有模板
+      this.loadTemplates();
+    } else {
+      let data = this.context.globalState.get('templates', []);
+      let stack = [...data];
+      let res = [];
+      while (stack.length) {
+        let node = stack.shift();
+        if (node.name.toLowerCase().includes(keyword.toLowerCase())) {
+          res.push(node);
+        }
+        if (node.hasChild) {
+          stack = [...stack, ...node?.children];
+        }
+      }
+      console.log('search res', res);
+      this.templates = res;
+    }
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTemplatesHandle(datas) {
+    let res = [...datas];
+    console.log('getTemplatesHandle res1', res);
+    for (let i = 0; i < res.length; i++) {
+      let hasChild = res[i].hasChild, children = res[i].children;
+      if (!res[i].children || (res[i].children instanceof Array && res[i].children.length === 0)) {
+        hasChild = false;
+        children = [];
+      } else {
+        children = this.getTemplatesHandle(res[i].children);
+      }
+      res[i] = {
+        ...res[i],
+        hasChild,
+        children
+      };
+    }
+    console.log('getTemplatesHandle res2', res);
+    return res;
+  }
+
+  getTemplates() {
+    if (this.templates.length) {
+      return this.getTemplatesHandle(this.templates);
+    } else {
+      return [
+        {
+          "id": "0",
+          "name": "demo1",
+          "content": "",
+          "parent": "",
+          "children": [
+            {
+              "id": "0-0",
+              "name": "demo1-child1",
+              "content": "child content 111",
+              "parent": "0",
+              "hasChild": false,
+              "children": []
+            },
+            {
+              "id": "0-1",
+              "name": "demo1-child2",
+              "content": "child content 222",
+              "parent": "0",
+              "hasChild": true,
+              "children": [
+                {
+                  "id": "0-1-0",
+                  "name": "demo1-child2-child1",
+                  "content": "demo content",
+                  "parent": "0-1",
+                  "hasChild": false,
+                  "children": []
+                }
+              ]
+            }
+          ],
+          "hasChild": true
+        }
+      ];
+    }
+  }
+
+  getSelectId() {
+    return this.selectId;
+  }
+
+  setSelectId(id) {
+    this.selectId = id;
+  }
+
+  getTemplatesOld() {
+    return this.templatesOld;
+  }
+}
+
+module.exports = { Template, TemplateDanceTreeDataProvider };
